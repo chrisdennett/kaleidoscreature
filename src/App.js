128aeb9a310a0ff04ac3a9a570e64bc50565bb63
chrisdennett <chrisdennett@gmail.com>
2020-07-19 14:52:02 +0100

    Added lint comments to remove warnings

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ export default function App() {
 
       setPolyHeight(newHeight);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [footPress]);
 
   const onCanvasClick = () => {
--- src/hooks/useKeyPress.js
+++ src/hooks/useKeyPress.js
@@ -27,6 +27,7 @@ export function useKeyPress(targetKey) {
       window.removeEventListener("keydown", downHandler);
       window.removeEventListener("keyup", upHandler);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []); // Empty array ensures that effect is only run on mount and unmount
 
   return keyPressed;
