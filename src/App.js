b220673358e1235310968e8292c3c3648c0c7ec8
chrisdennett <chrisdennett@gmail.com>
2020-07-19 14:22:15 +0100

    added shortcut key

--- src/App.js
+++ src/App.js
@@ -1,37 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 import { WebcamCapture } from "./WebcamCapture";
 import { useLocalStorage } from "./hooks/useLocalStorage";
+import { useKeyPress } from "./hooks/useKeyPress";
+
+const maxPolyHeight = 960;
+const minPolyHeight = 300;
 
 export default function App() {
   const [showControls, setShowControls] = useState(false);
-  const [numSegments, setNumSegments] = useState(6);
   const [polyHeight, setPolyHeight] = useLocalStorage("polyH", 400);
   const [yOffset, setYOffset] = useLocalStorage("yOffset", 0);
   const [xOffset, setXOffset] = useLocalStorage("xOffset", 0);
-  const [useSplitSegments, setUseSplitSegments] = useState(true);
+  const footPress = useKeyPress("h");
 
-  const incr = useSplitSegments ? 1 : 2;
+  useEffect(() => {
+    if (footPress) {
+      const polyHeightRounded = Math.round(polyHeight / 100) * 100;
+      const newHeight =
+        polyHeightRounded + 100 > maxPolyHeight
+          ? minPolyHeight
+          : polyHeightRounded + 100;
 
-  const increaseSegments = () => updateNumSegments(numSegments + incr);
-  const decreaseSegments = () => updateNumSegments(numSegments - incr);
-
-  const updateNumSegments = (value) => {
-    let newValue = value >= 3 ? value : 3;
-
-    setNumSegments(newValue);
-  };
-
-  const toggleUseSplitSegments = () => {
-    let newValue = !useSplitSegments;
-
-    // if going to a non-split setting don't allow an odd number of segments
-    if (newValue === false && numSegments % 2 !== 0) {
-      setNumSegments(numSegments + 1);
+      setPolyHeight(newHeight);
     }
-
-    setUseSplitSegments(newValue);
-  };
+  }, [footPress]);
 
   const onCanvasClick = () => {
     setShowControls(!showControls);
@@ -53,18 +46,11 @@ export default function App() {
     <div>
       {showControls && (
         <div style={{ position: "fixed" }}>
-          <button onClick={decreaseSegments}>-</button>
-          <button onClick={increaseSegments}>+</button>
-          {numSegments}
-          <button onClick={toggleUseSplitSegments}>
-            toggle split segments
-          </button>
-          {useSplitSegments ? "True" : "False"}
           <div>
             <input
               type="range"
-              min="100"
-              max="1000"
+              min={minPolyHeight}
+              max={maxPolyHeight}
               step="2"
               value={polyHeight}
               onChange={onPolyHeightSliderChange}
@@ -97,8 +83,8 @@ export default function App() {
       )}
       <WebcamCapture
         onClick={onCanvasClick}
-        numSegments={numSegments}
-        useSplitSegments={useSplitSegments}
+        numSegments={6}
+        useSplitSegments={true}
         polyHeight={polyHeight}
         yOffset={yOffset}
         xOffset={xOffset}
